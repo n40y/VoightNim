c417e2d167eb245b36bb22b33c77ff5575f2942b
N40y <131626661+n40y@users.noreply.github.com>
2026-07-12 15:29:38 +0200

    Create smtp.nim

---
+++ src/probes/smtp.nim
@@ -0,0 +1,17 @@
+# src/probes/smtp.nim
+
+import types
+import utils
+import ../signatures/smtp/init as smtpSignatures
+
+proc getSmtpProbe*(): ServiceProbe =
+  result = ServiceProbe(
+    probeType: ptSMTP,
+    name: "SMTP",
+    payload: toBytes("EHLO scanner.local\r\n"),
+    ports: @[25'u16, 465'u16, 587'u16],
+    timeoutMs: 1500,
+    rarity: 1,
+    ssl: false,
+    matches: smtpSignatures.getSmtpSignatures()
+  )
