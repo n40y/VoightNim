4aee6d471251c1c3851ce69c05ee23de8b4d62b0
N40y <131626661+n40y@users.noreply.github.com>
2026-07-12 20:10:35 +0200

    Update init.nim

--- src/signatures/ssh/init.nim
+++ src/signatures/ssh/init.nim
@@ -4,7 +4,7 @@
 ## Point d'entrée unique pour les signatures SSH.
 ## =============================================================================
 
-import ../../types
+import ../../fingerprint/types
 import ./servers
 
 proc getSshSignatures*(): seq[MatchRule] =
