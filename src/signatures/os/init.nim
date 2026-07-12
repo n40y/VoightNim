da569640e482df3166d72a30f0c03687ac9cddfb
N40y <131626661+n40y@users.noreply.github.com>
2026-07-12 15:34:05 +0200

    Create init.nim

---
+++ src/signatures/os/init.nim
@@ -0,0 +1,16 @@
+## =============================================================================
+## signatures/os/init.nim
+##
+## Point d'entrée unique pour les signatures OS. Même convention que
+## signatures/http/init.nim : un fichier par famille d'OS, agrégés ici.
+## =============================================================================
+
+import ../../types
+import ./linux
+import ./windows
+
+proc getOsSignatures*(): seq[OsMatchRule] =
+  result = @[]
+
+  result.add getLinuxOsSignatures()
+  result.add getWindowsOsSignatures()
